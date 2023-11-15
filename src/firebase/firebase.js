7fd6dfbcc7d6ea4e56156e2116ea24204a396fcd
Abdelrahman Gamal <abdelrhman.gamal53@gmail.com>
2023-11-15 02:14:42 +0200

    minor modifications

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,4 +15,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
+
 export const db = getFirestore();
